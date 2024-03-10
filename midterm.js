fbf128d88df6677afeaab77588a14915241034b0
mikufan76 <56976587+mikufan76@users.noreply.github.com>
2024-03-10 00:19:16 -0500

    noise

--- midterm.js
+++ midterm.js
@@ -1,98 +1,37 @@
 // You need to fill in the following functions.
-function drawArtwork() {}
+function drawArtwork() {
+    const brown = [119, 45, 18];
+    background(...brown);
+    setBround(.70);
+    setBround(.90);
+}
 
 function drawBackground() {}
-
-function randomGradient(ix, iy) {
-    // No precomputed gradients mean this works for any number of grid coordinates
-    const w = 8 * sizeof(unsigned);
-    const s = w / 2; // rotation width
-    let a, b;
-    (a = ix), (b = iy);
-    a *= 3284157443;
-    b ^= (a << s) | (a >> (w - s));
-    b *= 1911520717;
-    a ^= (b << s) | (b >> (w - s));
-    a *= 2048419325;
-    let random = a * (3.14159265 / ~(~0 >> 1)); // in [0, 2*Pi]
-    let v = { x: null, y: null };
-    v.x = Math.cos(random);
-    v.y = Math.sin(random);
-    return v;
-}
-
-// Computes the dot product of the distance and gradient vectors.
-function dotGridGradient(ix, iy, x, y) {
-    // Get gradient from integer coordinates
-    gradient = randomGradient(ix, iy);
-
-    // Compute the distance vector
-    dx = x - ix;
-    dy = y - iy;
-
-    // Compute the dot-product
-    return dx * gradient.x + dy * gradient.y;
-}
-
-// Algo from wiki
-function interpolate(a0, a1, w) {
-    /* // You may want clamping by inserting:
-     * if (0.0 > w) return a0;
-     * if (1.0 < w) return a1;
-     */
-    return (a1 - a0) * w + a0;
-    /* // Use this cubic interpolation [[Smoothstep]] instead, for a smooth appearance:
-     * return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
-     *
-     * // Use [[Smootherstep]] for an even smoother result with a second derivative equal to zero on boundaries:
-     * return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
-     */
-}
-
-// Computes the dot product of the distance and gradient vectors.
-function dotGridGradient(ix, iy, x, y) {
-    // Get gradient from integer coordinates
-    gradient = randomGradient(ix, iy);
-
-    // Compute the distance vector
-    dx = x - ix;
-    dy = y - iy;
-
-    // Compute the dot-product
-    return dx * gradient.x + dy * gradient.y;
-}
-
-// Compute Perlin noise at coordinates x, y
-function perlin(x, y) {
-    // Determine grid cell coordinates
-    x0 = Math.floor(x);
-    x1 = x0 + 1;
-    y0 = Math.floor(y);
-    y1 = y0 + 1;
-
-    // Determine interpolation weights
-    // Could also use higher order polynomial/s-curve here
-    sx = x - x0;
-    sy = y - y0;
-
-    // Interpolate between grid point gradients
-    let n0, n1, ix0, ix1, value;
-
-    n0 = dotGridGradient(x0, y0, x, y);
-    n1 = dotGridGradient(x1, y0, x, y);
-    ix0 = interpolate(n0, n1, sx);
-
-    n0 = dotGridGradient(x0, y1, x, y);
-    n1 = dotGridGradient(x1, y1, x, y);
-    ix1 = interpolate(n0, n1, sx);
-
-    value = interpolate(ix0, ix1, sy);
-    return value; // Will return in range -1 to 1. To make it in range 0 to 1, multiply by 0.5 and add 0.5
-}
 
 function drawWarhol() {}
 
 function drawZoom() {}
+
+function setBround(scale) {
+    const yellow = [241, 222, 112];
+    let noiseScale = 0.4;
+    let noisey = 1;
+    for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+            // Scale input coordinates.
+            let nx = noiseScale * x;
+            let ny = noiseScale * y;
+
+            // Compute noise value.
+            const alpha = noisey * noise(nx, ny);
+            if (alpha > scale) {
+                setPixel(x, y, yellow[0], yellow[1], yellow[2]);
+            }
+        }
+    }
+
+    describe('A gray cloudy pattern.');
+}
 
 function drawLine(x1, y1, x2, y2, r, g, b) {
     let x0 = x1,
