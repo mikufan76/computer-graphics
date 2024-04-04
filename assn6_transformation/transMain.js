9b1a405f9c327375e698921ec0bd2055a3d85471
mikufan76 <56976587+mikufan76@users.noreply.github.com>
2024-04-04 18:55:16 -0400

    box on screen

--- assn6_transformation/transMain.js
+++ assn6_transformation/transMain.js
@@ -1,12 +1,12 @@
-  'use strict';
+'use strict';
 
-  // Global variables that are set and used
-  // across the application
-  let gl, program;
-  
-  // Global declarations of objects that you will be drawing
-  var myTeapot = null;
+// Global variables that are set and used
+// across the application
+let gl, program;
 
+// Global declarations of objects that you will be drawing
+var myTeapot = null;
+let cube = null;
 
 //
 // A function that creates shapes to be drawn and creates a VAO for each
@@ -14,31 +14,28 @@
 // We start you out with an example for the teapot.
 //
 function createShapes() {
-
-    myTeapot = new Teapot();
-    myTeapot.VAO = bindVAO (myTeapot);
+    cube = new Cube(3);
+    cube.VAO = bindVAO(cube);
+    // myTeapot = new Teapot();
+    // myTeapot.VAO = bindVAO (myTeapot);
 }
-
 
 //
 // Set up your camera and your projection matrices
 //
 function setUpCamera() {
-    
     // set up your projection
     // defualt is orthographic projection
     let projMatrix = glMatrix.mat4.create();
     glMatrix.mat4.ortho(projMatrix, -5, 5, -5, 5, 1.0, 300.0);
-    gl.uniformMatrix4fv (program.uProjT, false, projMatrix);
+    gl.uniformMatrix4fv(program.uProjT, false, projMatrix);
 
-    
     // set up your view
     // defaut is at (0,0,-5) looking at the origin
     let viewMatrix = glMatrix.mat4.create();
     glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
-    gl.uniformMatrix4fv (program.uViewT, false, viewMatrix);
+    gl.uniformMatrix4fv(program.uViewT, false, viewMatrix);
 }
-
 
 //
 // Use this function to draw all of your shapes.
@@ -49,18 +46,26 @@ function setUpCamera() {
 // An example is shown for placing the teapot
 //
 function drawShapes() {
-    
-    
+    // let modelMatrix = glMatrix.mat4.create();
+
+    // // drawing the teapot rotating around Y  180 degrees
+    // glMatrix.mat4.rotateY (modelMatrix,  modelMatrix, radians(180.0))
+
+    // // send the model matrix to the shader and draw.
+    // gl.uniformMatrix4fv (program.uModelT, false, modelMatrix);
+    // gl.bindVertexArray(myTeapot.VAO);
+    // gl.drawElements(gl.TRIANGLES, myTeapot.indices.length, gl.UNSIGNED_SHORT, 0);
+
     let modelMatrix = glMatrix.mat4.create();
-    
+
     // drawing the teapot rotating around Y  180 degrees
-    glMatrix.mat4.rotateY (modelMatrix,  modelMatrix, radians(180.0))
-    
     // send the model matrix to the shader and draw.
-    gl.uniformMatrix4fv (program.uModelT, false, modelMatrix);
-    gl.bindVertexArray(myTeapot.VAO);
-    gl.drawElements(gl.TRIANGLES, myTeapot.indices.length, gl.UNSIGNED_SHORT, 0);
-    
+
+    const vec = vec3.fromValues(20, 0, 0);
+    // gl.mat4.translate(modelMatrix, modelMatrix, vec);
+    gl.uniformMatrix4fv(program.uModelT, false, modelMatrix);
+    gl.bindVertexArray(cube.VAO);
+    gl.drawElements(gl.TRIANGLES, cube.indices.length, gl.UNSIGNED_SHORT, 0);
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -69,22 +74,20 @@ function drawShapes() {
 //
 ///////////////////////////////////////////////////////////////////
 
-  // Given an id, extract the content's of a shader script
-  // from the DOM and return the compiled shader
-  function getShader(id) {
+// Given an id, extract the content's of a shader script
+// from the DOM and return the compiled shader
+function getShader(id) {
     const script = document.getElementById(id);
     const shaderString = script.text.trim();
 
     // Assign shader depending on the type of shader
     let shader;
     if (script.type === 'x-shader/x-vertex') {
-      shader = gl.createShader(gl.VERTEX_SHADER);
-    }
-    else if (script.type === 'x-shader/x-fragment') {
-      shader = gl.createShader(gl.FRAGMENT_SHADER);
-    }
-    else {
-      return null;
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else if (script.type === 'x-shader/x-fragment') {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else {
+        return null;
     }
 
     // Compile the shader using the supplied shader code
@@ -93,15 +96,15 @@ function drawShapes() {
 
     // Ensure the shader is valid
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.error(gl.getShaderInfoLog(shader));
-      return null;
+        console.error(gl.getShaderInfoLog(shader));
+        return null;
     }
 
     return shader;
-  }
+}
 
-  // Create a program with the appropriate vertex and fragment shaders
-  function initProgram() {
+// Create a program with the appropriate vertex and fragment shaders
+function initProgram() {
     const vertexShader = getShader('vertex-shader');
     const fragmentShader = getShader('fragment-shader');
 
@@ -113,7 +116,7 @@ function drawShapes() {
     gl.linkProgram(program);
 
     if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      console.error('Could not initialize shaders');
+        console.error('Could not initialize shaders');
     }
 
     // Use this program instance
@@ -122,52 +125,62 @@ function drawShapes() {
     // for easy access later in the code
     program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
     program.aBary = gl.getAttribLocation(program, 'bary');
-    program.uModelT = gl.getUniformLocation (program, 'modelT');
-    program.uViewT = gl.getUniformLocation (program, 'viewT');
-    program.uProjT = gl.getUniformLocation (program, 'projT');
-  }
+    program.uModelT = gl.getUniformLocation(program, 'modelT');
+    program.uViewT = gl.getUniformLocation(program, 'viewT');
+    program.uProjT = gl.getUniformLocation(program, 'projT');
+}
 
-  // creates a VAO and returns its ID
-  function bindVAO (shape) {
-      //create and bind VAO
-      let theVAO = gl.createVertexArray();
-      gl.bindVertexArray(theVAO);
-      
-      // create and bind vertex buffer
-      let myVertexBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.points), gl.STATIC_DRAW);
-      gl.enableVertexAttribArray(program.aVertexPosition);
-      gl.vertexAttribPointer(program.aVertexPosition, 4, gl.FLOAT, false, 0, 0);
-      
-      // create and bind bary buffer
-      let myBaryBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, myBaryBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.bary), gl.STATIC_DRAW);
-      gl.enableVertexAttribArray(program.aBary);
-      gl.vertexAttribPointer(program.aBary, 3, gl.FLOAT, false, 0, 0);
-      
-      // Setting up the IBO
-      let myIndexBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(shape.indices), gl.STATIC_DRAW);
+// creates a VAO and returns its ID
+function bindVAO(shape) {
+    //create and bind VAO
+    let theVAO = gl.createVertexArray();
+    gl.bindVertexArray(theVAO);
 
-      // Clean
-      gl.bindVertexArray(null);
-      gl.bindBuffer(gl.ARRAY_BUFFER, null);
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-      
-      return theVAO;
-    
-  }
+    // create and bind vertex buffer
+    let myVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(shape.points),
+        gl.STATIC_DRAW
+    );
+    gl.enableVertexAttribArray(program.aVertexPosition);
+    gl.vertexAttribPointer(program.aVertexPosition, 4, gl.FLOAT, false, 0, 0);
 
-  
-  // We call draw to render to our canvas
-  function draw() {
+    // create and bind bary buffer
+    let myBaryBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, myBaryBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(shape.bary),
+        gl.STATIC_DRAW
+    );
+    gl.enableVertexAttribArray(program.aBary);
+    gl.vertexAttribPointer(program.aBary, 3, gl.FLOAT, false, 0, 0);
+
+    // Setting up the IBO
+    let myIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
+    gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(shape.indices),
+        gl.STATIC_DRAW
+    );
+
+    // Clean
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    return theVAO;
+}
+
+// We call draw to render to our canvas
+function draw() {
     // Clear the scene
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-      
+
     // draw your shapes
     drawShapes();
 
@@ -175,48 +188,48 @@ function drawShapes() {
     gl.bindVertexArray(null);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-  }
+}
 
-  // Entry point to our application
-  function init() {
-      
+// Entry point to our application
+function init() {
     // Retrieve the canvas
     const canvas = document.getElementById('webgl-canvas');
     if (!canvas) {
-      console.error(`There is no canvas with id ${'webgl-canvas'} on this page.`);
-      return null;
+        console.error(
+            `There is no canvas with id ${'webgl-canvas'} on this page.`
+        );
+        return null;
     }
-
 
     // Retrieve a WebGL context
     gl = canvas.getContext('webgl2');
     if (!gl) {
         console.error(`There is no WebGL 2.0 context`);
         return null;
-      }
-      
+    }
+
     // Set the clear color to be black
     gl.clearColor(0, 0, 0, 1);
-      
+
     // some GL initialization
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-    
+
     gl.cullFace(gl.BACK);
     gl.frontFace(gl.CCW);
-    gl.clearColor(0.0,0.0,0.0,1.0)
-    gl.depthFunc(gl.LEQUAL)
-    gl.clearDepth(1.0)
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clearDepth(1.0);
 
     // Read, compile, and link your shaders
     initProgram();
-    
+
     // create and bind your current object
     createShapes();
-    
+
     // set up your camera
     setUpCamera();
-    
+
     // do a draw
     draw();
-  }
+}
