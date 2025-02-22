c6a4788fb1e2e867d862bcd05edb7c4299a624bd
Orion DiLorenzo <56976587+mikufan76@users.noreply.github.com>
2025-02-22 01:17:39 -0500

    Spheres in rust!!!!!!!!

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -2,12 +2,13 @@ mod shapes;
 
 use crate::shapes::Shape;
 use image;
-use nalgebra::Vector3;
+use nalgebra::{distance, Vector3};
 
 const WIDTH: usize = 500;
 const HEIGHT: usize = 300;
 const FOCAL_LENGTH: f32 = 100.0;
 const ORIGIN: Vector3<f32> = Vector3::new(1.0, 0.0, 0.0);
+// const ORIGIN: Vector3<f32> = Vector3::new(1.0, -10.0, -19.0);
 const BG: image::Rgb<u8> = image::Rgb([62, 144, 235]);
 
 fn main() {
@@ -28,7 +29,22 @@ fn main() {
         image::Rgb([0, 0, 0]),
     );
 
-    let shapes = vec![tri_1, tri_2];
+    let sphere_1 = shapes::create_sphere(
+        Vector3::new(0.0, -6.0, 20.0),
+        12.0,
+        image::Rgb([100, 255, 255]),
+        image::Rgb([203, 238, 248]),
+    );
+
+    let sphere_2 = shapes::create_sphere(
+        Vector3::new(15.0, 4.0, 25.0),
+        12.0,
+        image::Rgb([0, 238, 248]),
+        image::Rgb([203, 238, 248]),
+    );
+
+    let tris = vec![tri_1, tri_2];
+    let spheres = vec![sphere_1, sphere_2];
 
     let mut imgbuf = image::ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
 
@@ -44,11 +60,11 @@ fn main() {
         *pixel = image::Rgb([r, 0, b]);
         let x1 = x as f32 - width / 2.0;
         let y1 = y as f32 - height / 2.0;
-        let direction: Vector3<f32> = Vector3::new(x1, y1, FOCAL_LENGTH);
+        let direction: Vector3<f32> = Vector3::new(x1, y1, FOCAL_LENGTH).normalize();
 
         let mut closest_dist = f32::INFINITY;
         let mut collided = false;
-        for tri in shapes.iter() {
+        for tri in tris.iter() {
             let dist = tri.collide(ORIGIN, direction);
 
             if dist < closest_dist {
@@ -58,10 +74,18 @@ fn main() {
             }
         }
 
+        for sphere in spheres.iter() {
+            let dist = sphere.collide(ORIGIN, direction);
+            if dist < closest_dist {
+                closest_dist = dist;
+                diffuse = sphere.diffuse;
+                collided = true;
+            }
+        }
+
         if collided {
             *pixel = diffuse;
-        }
-        else {
+        } else {
             *pixel = BG;
         }
     }
--- renderer/src/shapes.rs
+++ renderer/src/shapes.rs
@@ -1,4 +1,6 @@
+use image::Rgb;
 use nalgebra::{ComplexField, Vector3};
+use std::f32;
 
 pub trait Shape {
     fn collide(&self, origin: Vector3<f32>, direction: Vector3<f32>) -> f32;
@@ -15,15 +17,53 @@ pub fn generic_diffuse(s: &dyn Shape) -> image::Rgb<u8> {
 pub struct Sphere {
     position: Vector3<f32>,
     radius: f32,
-    diffuse: image::Rgb<u8>,
+    pub(crate) diffuse: image::Rgb<u8>,
     specular: image::Rgb<u8>,
 }
 
-// impl Sphere {
-//     fn collision(&self, origin: &Vector3<f32>, direction: &Vector3<f32>) -> Vector3<f32> {
-//         let v0v1 =  Vector3::new()
-//     }
-// }
+pub fn create_sphere(
+    position: Vector3<f32>,
+    radius: f32,
+    diffuse: image::Rgb<u8>,
+    specular: image::Rgb<u8>,
+) -> Sphere {
+    return Sphere {
+        position,
+        radius,
+        diffuse,
+        specular,
+    };
+}
+
+fn distance(x: f32, y: f32) -> f32 {
+    (x * x - y * y).sqrt()
+}
+impl Shape for Sphere {
+    fn collide(&self, origin: Vector3<f32>, direction: Vector3<f32>) -> f32 {
+        // let rayToSphere = math.sub(sphere.position, origin);
+        let ray_to_sphere = self.position - origin;
+        // let distRayToSphere = math.mag(rayToSphere);
+        let dist_ray_to_sphere = ray_to_sphere.magnitude();
+        // let distClosestPointOnRay = math.dot(rayToSphere, direction);
+        let dist_closest_point_on_ray = ray_to_sphere.dot(&direction);
+        // let distClosestPointToSphere = Math.sqrt(distRayToSphere ** 2 - distClosestPointOnRay ** 2);
+        let dist_closest_point_to_sphere = distance(dist_ray_to_sphere, dist_closest_point_on_ray);
+        // let distToIntersection = distClosestPointOnRay -  Math.sqrt(sphere.radius ** 2 - distClosestPointToSphere ** 2);
+        let distance_to_intersection = dist_closest_point_on_ray - distance(self.radius, dist_closest_point_to_sphere);
+        if (dist_closest_point_on_ray > 0.0 && dist_closest_point_to_sphere < self.radius) {
+            return distance_to_intersection;
+        }
+        f32::INFINITY
+    }
+
+    fn get_diffuse(&self) -> Rgb<u8> {
+        self.diffuse
+    }
+
+    fn get_specular(&self) -> Rgb<u8> {
+        todo!()
+    }
+}
 
 pub fn create_triangle(
     v0: Vector3<f32>,
@@ -75,7 +115,6 @@ impl Shape for Triangle {
         let v0p = point - self.v0;
         let c1 = self.v1v0.cross(&v0p).dot(&normal) >= 0.0;
 
-
         let v1p = point - self.v1;
         let c2 = self.v2v1.cross(&v1p).dot(&normal) > 0.0;
 
@@ -83,7 +122,6 @@ impl Shape for Triangle {
         let c3 = self.v0v2.cross(&v2p).dot(&normal) >= 0.0;
 
         if (c1 && c2 && c3) {
-
             return dist_to_surface;
         }
         return f32::INFINITY;
