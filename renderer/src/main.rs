7a1526d314e4e615aabeb3d276825bd9ceb0a3da
Orion DiLorenzo <56976587+mikufan76@users.noreply.github.com>
2025-02-21 22:39:50 -0500

    triangles in rust!!!

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -1,76 +1,70 @@
 mod shapes;
 
-use crate::shapes::{Shape, generic_collide, generic_diffuse};
-use minifb::KeyRepeat::No;
-use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
+use crate::shapes::Shape;
+use image;
 use nalgebra::Vector3;
-use std::ptr::null;
 
 const WIDTH: usize = 500;
 const HEIGHT: usize = 300;
 const FOCAL_LENGTH: f32 = 100.0;
 const ORIGIN: Vector3<f32> = Vector3::new(1.0, 0.0, 0.0);
+const BG: image::Rgb<u8> = image::Rgb([62, 144, 235]);
 
 fn main() {
     let mut buffer = vec![0u32; WIDTH * HEIGHT];
-
-    let mut window = Window::new(
-        "Ray tracer",
-        WIDTH,
-        HEIGHT,
-        WindowOptions {
-            resize: true,
-            scale: Scale::X4,
-            scale_mode: ScaleMode::AspectRatioStretch,
-            ..WindowOptions::default()
-        },
-    )
-    .expect("Unable to create the window");
-
-    window.set_target_fps(60);
 
     let tri_1 = shapes::create_triangle(
         Vector3::new(50.0, 20.0, 10.0),
         Vector3::new(-30.0, 20.0, 10.0),
         Vector3::new(-50.0, 20.0, 60.0),
-        Vector3::new(203, 0, 0),
-        Vector3::new(0, 0, 0),
+        image::Rgb([203, 0, 0]),
+        image::Rgb([0, 0, 0]),
     );
     let tri_2 = shapes::create_triangle(
         Vector3::new(50.0, 20.0, 10.0),
         Vector3::new(80.0, 20.0, 60.0),
         Vector3::new(-50.0, 20.0, 60.0),
-        Vector3::new(203, 0, 0),
-        Vector3::new(0, 0, 0),
+        image::Rgb([203, 0, 0]),
+        image::Rgb([0, 0, 0]),
     );
 
     let shapes = vec![tri_1, tri_2];
 
-    window.set_background_color(0, 0, 20);
-    let mut diffuse: Vector3<u32> = Vector3::new(0, 0, 0);
+    let mut imgbuf = image::ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        for (i, pixel) in buffer.iter_mut().enumerate() {
-            let x = (i % WIDTH) as f32;
-            let y = (i / WIDTH) as f32;
-            let direction = Vector3::new(x, y, FOCAL_LENGTH);
+    let mut diffuse: image::Rgb<u8> = image::Rgb([0, 0, 0]);
 
-            let mut closest_dist = f32::INFINITY;
-            let mut collided = false;
-            for tri in shapes.iter() {
-                let dist = tri.collide(ORIGIN, direction);
+    let width = WIDTH as f32;
+    let height = HEIGHT as f32;
 
-                if (dist < closest_dist) {
-                    closest_dist = dist;
-                    diffuse = tri.diffuse;
-                    collided = true;
-                }
-            }
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+        let x1 = x as f32 - width / 2.0;
+        let y1 = y as f32 - height / 2.0;
+        let direction: Vector3<f32> = Vector3::new(x1, y1, FOCAL_LENGTH);
 
-            if (collided) {
-                *pixel = diffuse[0] | diffuse[1] | diffuse[2];
+        let mut closest_dist = f32::INFINITY;
+        let mut collided = false;
+        for tri in shapes.iter() {
+            let dist = tri.collide(ORIGIN, direction);
+
+            if dist < closest_dist {
+                closest_dist = dist;
+                diffuse = tri.diffuse;
+                collided = true;
             }
         }
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+
+        if collided {
+            *pixel = diffuse;
+        }
+        else {
+            *pixel = BG;
+        }
     }
+
+    imgbuf.save("trace.png").unwrap();
 }
--- renderer/src/shapes.rs
+++ renderer/src/shapes.rs
@@ -2,23 +2,21 @@ use nalgebra::{ComplexField, Vector3};
 
 pub trait Shape {
     fn collide(&self, origin: Vector3<f32>, direction: Vector3<f32>) -> f32;
-    fn get_diffuse(&self)-> Vector3<u32>;
-    fn get_specular(&self)-> Vector3<u32>;
-
-
+    fn get_diffuse(&self) -> image::Rgb<u8>;
+    fn get_specular(&self) -> image::Rgb<u8>;
 }
 
 pub fn generic_collide(s: &dyn Shape, origin: Vector3<f32>, direction: Vector3<f32>) -> f32 {
     s.collide(origin, direction)
 }
-pub fn generic_diffuse(s: &dyn Shape) -> Vector3<u32> {
+pub fn generic_diffuse(s: &dyn Shape) -> image::Rgb<u8> {
     s.get_diffuse()
 }
 pub struct Sphere {
     position: Vector3<f32>,
     radius: f32,
-    diffuse: Vector3<f32>,
-    specular: Vector3<f32>,
+    diffuse: image::Rgb<u8>,
+    specular: image::Rgb<u8>,
 }
 
 // impl Sphere {
@@ -31,14 +29,14 @@ pub fn create_triangle(
     v0: Vector3<f32>,
     v1: Vector3<f32>,
     v2: Vector3<f32>,
-    diffuse: Vector3<u32>,
-    specular: Vector3<u32>,
+    diffuse: image::Rgb<u8>,
+    specular: image::Rgb<u8>,
 ) -> Triangle {
     let v0v2 = v0 - v2;
     let v1v0 = v1 - v0;
     let v2v0 = v2 - v0;
     let v2v1 = v2 - v1;
-    let normal = v1v0.cross(&v0v2).normalize();
+    let normal = v1v0.cross(&v2v0).normalize();
 
     Triangle {
         v0,
@@ -58,8 +56,8 @@ pub struct Triangle {
     pub v0: Vector3<f32>,
     pub v1: Vector3<f32>,
     pub v2: Vector3<f32>,
-    pub diffuse: Vector3<u32>,
-    pub specular: Vector3<u32>,
+    pub diffuse: image::Rgb<u8>,
+    pub specular: image::Rgb<u8>,
     pub normal: Vector3<f32>,
     pub v0v2: Vector3<f32>,
     pub v1v0: Vector3<f32>,
@@ -74,21 +72,53 @@ impl Shape for Triangle {
         let dist_to_surface = (plane_offset - normal.dot(&origin)) / normal.dot(&direction);
         let point = direction.scale(dist_to_surface) + origin;
 
-        let c1 = self.v1v0.cross(&(point - self.v0)).dot(&self.normal) >= 0.0;
-        let c2 = self.v2v1.cross(&(point - self.v1)).dot(&self.normal) >= 0.0;
-        let c3 = self.v0v2.cross(&(point - self.v2)).dot(&self.normal) >= 0.0;
+        let v0p = point - self.v0;
+        let c1 = self.v1v0.cross(&v0p).dot(&normal) >= 0.0;
+
+
+        let v1p = point - self.v1;
+        let c2 = self.v2v1.cross(&v1p).dot(&normal) > 0.0;
+
+        let v2p = point - self.v2;
+        let c3 = self.v0v2.cross(&v2p).dot(&normal) >= 0.0;
 
         if (c1 && c2 && c3) {
+
             return dist_to_surface;
         }
         return f32::INFINITY;
+
+        // let mut ne;
+        //
+        // let v0p = point - self.v0;
+        // ne = self.v1v0.cross(&v0p);
+        // if (ne.dot(&normal) >= 0.0) // point is on right side
+        // {
+        //     return f32::INFINITY;
+        // }
+        //
+        // let v1p = point - self.v1;
+        // ne = self.v2v1.cross(&v1p);
+        // if (ne.dot(&normal) >= 0.0) // point is on right side
+        // {
+        //     return f32::INFINITY;
+        // }
+        //
+        // let v2p = point - self.v2;
+        // ne = self.v2v0.cross(&v2p);
+        // if (ne.dot(&normal) >= 0.0) // point is on right side
+        // {
+        //     return f32::INFINITY;
+        // }
+        //
+        // dist_to_surface
     }
 
-    fn get_diffuse(&self) -> Vector3<u32> {
+    fn get_diffuse(&self) -> image::Rgb<u8> {
         self.diffuse
     }
 
-    fn get_specular(&self) -> Vector3<u32> {
+    fn get_specular(&self) -> image::Rgb<u8> {
         self.specular
     }
 }
